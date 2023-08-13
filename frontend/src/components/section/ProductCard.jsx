04bb7bffabf2f8da84e70fd417749791cd693b1f
Diki Siswanto <dickyachmady@gmail.com>
2023-08-13 23:52:30 +0800

    chore: add lazy loading in product photos

--- frontend/src/components/section/ProductCard.jsx
+++ frontend/src/components/section/ProductCard.jsx
@@ -16,6 +16,7 @@ export default function ProductCard({ product, className }) {
           src={product.photos[0]}
           alt={product.title}
           className="h-24 w-full object-cover object-center"
+          loading="lazy"
         />
       </figure>
       <div className="px-2 py-1 inline-flex flex-col gap-1 text-xs">
